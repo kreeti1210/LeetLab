a3e39ae652b6324daf0ddb66d0d8d358f837b89e
kreeti1210 <kreetisharma1210@gmail.com>
2025-06-20 23:59:02 +0530

    added token

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -47,6 +47,7 @@ export const useAuthStore = create((set) => ({
     set({ isLoggingIn: true });
     try {
       const res = await axiosInstance.post("/auth/login", data);
+      localStorage.setItem("token", res.data.token);
 
       set({ authUser: res.data.user });
 
