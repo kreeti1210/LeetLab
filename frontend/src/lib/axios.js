b58214a6d0d0305ba4a30a33a04321db5780ce8e
kreeti1210 <kreetisharma1210@gmail.com>
2025-06-20 22:50:29 +0530

    changes to env file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8080/api/v1"
-      : "/api/v1",
+      : `${import.meta.env.VITE_BACKEND_URL}/api/v1`,
   withCredentials: true,
 });
